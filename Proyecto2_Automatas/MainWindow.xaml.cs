1090a7c272b2d37443afb23bd67d8a409546fa24
David Berrospe Ramírez <david.berrospe.r03@gmail.com>
2020-06-17 06:35:20 -0500

    Added handling of new states. Pending AFD table insertion and testing.

--- Proyecto2_Automatas/MainWindow.xaml.cs
+++ Proyecto2_Automatas/MainWindow.xaml.cs
@@ -30,6 +30,9 @@ namespace Proyecto2_Automatas
         DataTable initTable, finalTable;
         Queue<NewStateHelper> createdStates;
         Queue<string> qInitialStates;
+        Queue<NewStateHelper> qPendingStates;
+        Queue<NewStateHelper> qGeneratedStates;
+        NewState[] AFD;
 
         public MainWindow()
         {
@@ -120,6 +123,9 @@ namespace Proyecto2_Automatas
             }
             i = 0;
             state = stateOrder(state.Length);
+
+            initMainStates();
+
             while (i < state.Length)
             {
                 DataRow tableRow;
@@ -156,11 +162,24 @@ namespace Proyecto2_Automatas
 
         private void InitAfd()
         {
-            primaryStateTransitions = new Dictionary<string, Dictionary<string, string[]>>();
+            // Obtener información de la tabla
+            getAFNDData();
+
+            // Encolamos los estados que se generan a partir de la data inicial
+            setPendingStates();
+
+            generateAFD( );
+        }
+
+        private void getAFNDData( )
+        {
+            primaryStateTransitions = new Dictionary<string, Dictionary<string, string[]>>(); // Inicialización del diccionario de la data inicial
+
             for (int i = 0; i < state.Length; i++)
             {
-                Dictionary<string, string[]> tempDictionaryTransitions = new Dictionary<string, string[]>();
-                primaryStateTransitions.Add(state[i], tempDictionaryTransitions);
+                Dictionary<string, string[]> tempDictionaryTransitions = new Dictionary<string, string[]>(); // Diccionario temporal
+                primaryStateTransitions.Add(state[i], tempDictionaryTransitions); // Diccionario para guardar la data de la tabla
+
                 for (int j = 0; j < alphabet.Length; j++)
                 {
                     string tempStr = initTable.Rows[i].Field<string>(j + 1);
@@ -177,7 +196,11 @@ namespace Proyecto2_Automatas
                     primaryStateTransitions[state[i]].Add(alphabet[j], tempTransitions);
                 }
             }
-            Queue<NewStateHelper> qPendingStates = new Queue<NewStateHelper>();
+        }
+
+        private void setPendingStates()
+        {
+            qPendingStates = new Queue<NewStateHelper>();
             foreach (string stateName in qInitialStates)
             {
                 foreach (string alphabetElement in alphabet)
@@ -188,22 +211,27 @@ namespace Proyecto2_Automatas
                         newStateHelper.Name = String.Join("", primaryStateTransitions[stateName][alphabetElement]);
                         newStateHelper.baseStates = primaryStateTransitions[stateName][alphabetElement];
                         newStateHelper.isFinal = false;
+                        newStateHelper.baseStatesAsDict = new Dictionary<string, bool>();
+
                         foreach (string state in primaryStateTransitions[stateName][alphabetElement])
                         {
+                            // Adding states dictionary
+                            newStateHelper.baseStatesAsDict.Add(state, true);
+
                             if (statesClasification[state] == 2)
                             {
                                 newStateHelper.isFinal = true;
                             }
                         }
                         qPendingStates.Enqueue(newStateHelper);
+
                     }
                 }
             }
-            insertData(qPendingStates);
         }
-
-        private void insertData(Queue<NewStateHelper> pendingStates)
+        private void generateAFD( )
         {
+            // Insertamos columnas ----------------------------------------
             finalTable.Columns.Add("Estado", typeof(string));
             int i = 0, actualRow = 0;
             while (i < alphabet.Length)
@@ -211,25 +239,115 @@ namespace Proyecto2_Automatas
                 finalTable.Columns.Add(alphabet[i], typeof(string));
                 i++;
             }
-            while (pendingStates.Count > 0)
+            // ------------------------------------------------------------
+
+            while (qPendingStates.Count > 0)
             {
-                NewStateHelper temp = pendingStates.Dequeue();
-                foreach (string baseState in temp.baseStates)
+                NewStateHelper temp = qPendingStates.Dequeue();
+                if( !newStateExists(temp) ) // Si el estado no existe
                 {
-                    foreach (string alphabetElement in alphabet)
+                    generateStateTransitions(temp);
+                }
+            }
+
+
+
+
+
+
+
+        }
+
+        public void generateStateTransitions(NewStateHelper newState)
+        {
+            NewState tempState = new NewState();
+            
+            tempState.Name = newState.Name;
+            tempState.baseStates = newState.baseStates;
+            tempState.baseStatesAsDict = newState.baseStatesAsDict;
+            tempState.transitions = new Dictionary<string, string>();
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                NewStateHelper possibleNewState = new NewStateHelper();
+                string alphabetElem = alphabet[i];
+                string[] tempTransitions = new string[] { };
+                int transitionIndex = 0;
+
+                Dictionary<string, bool> tempTransitionsDict = new Dictionary<string, bool>();
+
+                for (int j = 0; j < newState.baseStates.Length; j++) // Itera sobre las transiciones que generan el nuevo estado
+                {
+                    string baseTransitionName = newState.baseStates[j];
+                    string[] baseTransitionTransitions = primaryStateTransitions[baseTransitionName][alphabetElem];
+
+                    for(int k = 0; k < baseTransitionTransitions.Length; k++)
                     {
-                        string[] temporalTransitionsStates = primaryStateTransitions[baseState][alphabetElement];
-                        string newTransition = "";
-                        foreach (string item in temporalTransitionsStates)
+                        string transition = baseTransitionTransitions[k];
+                        bool dummyVal = false;
+
+                        // Verificar si existe la transición en las transiciones usadas, sino la agregamos a la lista
+                        if(!tempTransitionsDict.TryGetValue(transition, out dummyVal))
                         {
-                            if (newTransition.IndexOf(item) == -1)
-                            {
-                                newTransition += item;
-                            }
+                            tempTransitionsDict.Add(transition, true);
+                            tempTransitions[transitionIndex] = transition;
+                            transitionIndex++;
                         }
                     }
                 }
+
+                string newStateName = String.Join("", tempTransitions);
+                tempState.transitions.Add(alphabetElem, newStateName);
+
+                possibleNewState.Name = newStateName;
+                possibleNewState.baseStates = tempTransitions;
+                possibleNewState.baseStatesAsDict = tempTransitionsDict;
+
+                qPendingStates.Enqueue(possibleNewState);
             }
+
+            // Se agrega el nuevo estado a la lista de nuevos estados
+            AFD[AFD.Length] = tempState;
+        }
+
+        private void initMainStates(  )
+        {
+            foreach(string stateName in state)
+            {
+                NewStateHelper tempState = new NewStateHelper();
+                string[] tempTransitions = new string[] { stateName };
+
+                tempState.Name = stateName;
+                tempState.baseStates = tempTransitions;
+                tempState.isFinal = statesClasification[stateName] == 2;
+                
+                // Add states to dictionary
+                tempState.baseStatesAsDict = new Dictionary<string, bool>();
+                tempState.baseStatesAsDict.Add(stateName, true);
+
+                qGeneratedStates.Enqueue(tempState);
+            }
+        }
+
+        private bool newStateExists( NewStateHelper preState )
+        {
+            foreach (NewStateHelper stateGenerated in qGeneratedStates) 
+            {
+                if (preState.baseStates.Length == stateGenerated.baseStates.Length)
+                {
+                    int i = preState.baseStates.Length;
+                    foreach(string baseState in preState.baseStates)
+                    {
+                        if(stateGenerated.isTransitionUsedForStateCreation(baseState)) // Verifica si la transición del nuevo estado existe en las transiciones que se usaron como base para el estado ya existente
+                        {
+                            i--;
+                        }
+                    }
+                    if (i == 0) return true;
+                }
+            }
+
+            return false;
         }
     }
 
@@ -238,6 +356,18 @@ namespace Proyecto2_Automatas
         public string Name { get; set; }
         public string[] baseStates { get; set; }
         public bool isFinal { get; set; }
+        public Dictionary<string, bool> baseStatesAsDict { get; set; }
+
+        public bool isTransitionUsedForStateCreation(string baseTransition)
+        {
+            bool val = false;
+            return this.baseStatesAsDict.TryGetValue(baseTransition, out val);
+        }
+    }
+
+    public class NewState : NewStateHelper
+    {
+        public Dictionary<string, string> transitions;
     }
 
     //public class Transtition
